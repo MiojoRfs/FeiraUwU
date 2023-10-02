44ec5c813e0603dd5a1e321cbdde1a44c98b4774
Rafael <Rafael@DESKTOP-JOBU0Q8>
2023-10-02 20:20:05 -0300

    Adicionar arquivos de projeto.

---
+++ Feira de ciências/Form1.Designer.cs
@@ -0,0 +1,67 @@
+﻿namespace Feira_de_ciências
+{
+    partial class TelaInicial
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(TelaInicial));
+            this.BTiniciar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // BTiniciar
+            // 
+            this.BTiniciar.Location = new System.Drawing.Point(51, 375);
+            this.BTiniciar.Name = "BTiniciar";
+            this.BTiniciar.Size = new System.Drawing.Size(122, 41);
+            this.BTiniciar.TabIndex = 0;
+            this.BTiniciar.Text = "button1";
+            this.BTiniciar.UseVisualStyleBackColor = true;
+            // 
+            // TelaInicial
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.CadetBlue;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(957, 695);
+            this.Controls.Add(this.BTiniciar);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "TelaInicial";
+            this.Text = ":::: inicial ::::";
+            this.Load += new System.EventHandler(this.TelaInicial_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BTiniciar;
+    }
+}
+
---
+++ Feira de ciências/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Feira_de_ciências
+{
+    public partial class TelaInicial : Form
+    {
+        public TelaInicial()
+        {
+            InitializeComponent();
+        }
+
+        private void TelaInicial_Load(object sender, EventArgs e)
+        {
+          
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
